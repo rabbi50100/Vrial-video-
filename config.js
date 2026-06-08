62cff21dc82af1263b9cff597827b277dd8d61eb
rabbi50100 <mamatol605@gmail.com>
2026-06-08 20:41:48 +0600

    Add configuration for API and Firebase

---
+++ config.js
@@ -0,0 +1,13 @@
+export const API_BASE = window.location.hostname === 'localhost'
+  ? 'http://localhost:3000'
+  : 'https://developer-ambien-date-facilitate.trycloudflare.com';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBEePBfwMrVLNtapYGfpfsN-YWi3_K7Mv4",
+  authDomain: "vidgrab-pro.firebaseapp.com",
+  databaseURL: "https://vidgrab-pro-default-rtdb.firebaseio.com",
+  projectId: "vidgrab-pro",
+  storageBucket: "vidgrab-pro.firebasestorage.app",
+  messagingSenderId: "510494077769",
+  appId: "1:510494077769:web:cc33426f8e44e794ab2098"
+};
