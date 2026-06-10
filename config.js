955e7694f1846dbd19eb7fc8f04a78096d49b8aa
rabbi50100 <mamatol605@gmail.com>
2026-06-11 00:34:24 +0600

    Update API_BASE URL for production environment

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const API_BASE = window.location.hostname === 'localhost'
   ? 'http://localhost:3000'
-  : 'https://promo-knit-networking-handling.trycloudflare.com';
+  : 'https://realm-physician-social-update.trycloudflare.com';
 
 export const firebaseConfig = {
   apiKey: "AIzaSyBEePBfwMrVLNtapYGfpfsN-YWi3_K7Mv4",
